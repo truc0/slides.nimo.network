9b3d44a848e3740d1ed7fa53e74a39afbc92fed8
truc0 <22969604+truc0@users.noreply.github.com>
2024-04-09 02:26:37 +0800

    feat: add toc

--- build.js
+++ build.js
@@ -4,31 +4,42 @@ const path = require('path')
 
 const directory = __dirname
 const dist = path.join(__dirname, 'dist')
+const toc = path.join(__dirname, 'toc.md')
 
-const slideFilenames = fs.readdirSync(directory).filter(item => {
-    if (!item.startsWith('lesson-') || !item.endsWith('.md')) return false
-    if (fs.statSync(item).isDirectory()) return false
-    return true
-})
-console.log(slideFilenames)
+const buildSlides = () => {
+    const slideFilenames = fs.readdirSync(directory).filter(item => {
+        if (!item.startsWith('lesson-') || !item.endsWith('.md')) return false
+        if (fs.statSync(item).isDirectory()) return false
+        return true
+    })
+
+    slideFilenames.forEach(filename => {
+        // extract lession number
+        const slideName = filename.replace(".md", "")
+        const command = `yarn build ${filename} --out ${dist}/${slideName} --base /${slideName}/`
+        console.log(command)
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(error)
+                console.error(stderr)
+            } else {
+                console.log(stdout)
+            }
+        })
+    })
+}
 
 // clean up dist
 if (fs.existsSync(dist)) {
     fs.rmSync(dist, { force: true, recursive: true })
 }
 
-slideFilenames.forEach(filename => {
-    // extract lession number
-    const slideName = filename.replace(".md", "")
-    const command = `yarn build ${filename} --out ${dist}/${slideName} --base /${slideName}/`
-    console.log(command)
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(error)
-            console.error(stderr)
-        } else {
-            console.log(stdout)
-        }
-    })
-})
+// build toc first
+exec(`yarn build toc.md`, (error, stdout, stderr) => {
+    if (error) console.error(error)
+    if (stderr) console.error(stderr)
+    console.log(stdout)
 
+    // must run after toc built
+    buildSlides()
+})
---
+++ toc.md
@@ -0,0 +1,37 @@
+---
+# try also 'default' to start simple
+theme: seriph 
+# apply any windi css classes to the current slide
+class: 'text-center'
+# https://sli.dev/custom/highlighters.html
+highlighter: shiki
+# show line numbers in code blocks
+lineNumbers: true
+# some information about the slides, markdown enabled
+info: NIMO 2021 Web Tutorial Slides Table of Content
+# persist drawings in exports and build
+drawings:
+  persist: true
+background: 'https://source.unsplash.com/collection/94734566/1920x1080'
+---
+
+# NIMO 2021 Web Tutorial
+
+Table of Content of Lecture Notes
+
+---
+
+# Table of Content
+
+- [Lesson 01 Web Basics](/lesson-01/)
+- [Lesson 02 HTTP & Linux Operation Basics](/lesson-02/)
+- [Lesson 03 More CSS Settings](/lesson-03/)
+- [Lesson 04 The JavaScript Language](/lesson-04/)
+- [Lesson 05 How to build a website?](/lesson-05/)
+- [Lesson 07 Vue.js Basics](/lesson-07/)
+- [Lesson 08 Getting data from the Internet](/lesson-08/)
+- [Lesson 09 Python Basics](/lesson-09/)
+- [Lesson 10 Python Basics - Module and Class](/lesson-10/)
+- [Lesson 11 Python Basics - Class and Exception](/lesson-11/)
+- [Lesson 12 Hi, Django](/lesson-12/)
+- [Lesson 13 Forms and Views in Django](/lesson-13/)
